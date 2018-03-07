e971083ec74681ab936888984a57e78004de00b4
mmxals <kttr@communia.org>
2018-03-07 13:28:57 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+apt install python-qt4
 cp candela_show.py /usr/local/bin/candela_show.py
 cp logo_candela.png /usr/local/share/logo_candela.png
-cp Candela\ Show.desktop /etc/ /etc/guest-session/skel/Escritorio/Candela\ Show.desktop
+cp candela-show.desktop /usr/share/applications/candela-show.desktop
 wget http://www.ateneucandela.info/presentacio/img/logo1negatiu.jpg -P /usr/share/wallpapers
