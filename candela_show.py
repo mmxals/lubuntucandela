559a60ff5d5ab660577c19b312b7d88e3ed31fb7
mmxals <kttr@communia.org>
2018-03-07 15:34:19 +0100

    Update candela_show.py

--- candela_show.py
+++ candela_show.py
@@ -1,41 +1,28 @@
-#!/usr/bin/python
-import os,sys
-from PyQt4.QtCore import *
-from PyQt4.QtGui import *
-from PyQt4.QtWebKit import *
-from PyQt4 import QtGui, QtCore
+from PyQt5.QtCore import QUrl
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWebKitWidgets import QWebView 
 
-class browser(QtGui.QWidget):
-   
+class MainWindow(QMainWindow):
     def __init__(self):
-        QtGui.QMainWindow.__init__(self)
-
-        self.web = QWebView()
+        super(MainWindow, self).__init__()
+        self.web = QWebView(self)
+        self.web.showFullScreen()
         self.web.load(QUrl("http://www.ateneucandela.info/presentacio/index.html"))
-        self.initUI()
+        self.setCentralWidget(self.web)
 
-    def initUI(self):
-
-        layout = QStackedLayout()
-        layout.addWidget(self.web)
-        self.setLayout(layout)
-        self.showFullScreen()
-       
     def keyPressEvent(self, eventkey):
-      key = eventkey.key()
-      if key:
-         self.close()
+        key = eventkey.key()
+        if key:
+            self.close()
         
-    def mouseMoveEvent(self, eventmouse):
-      mouse = eventmouse.pos()
-      if mouse:
-        self.close()
-
-def main():
-    app = QtGui.QApplication(sys.argv)
-    ex = browser()
-    sys.exit(app.exec_())
-
-
 if __name__ == '__main__':
-    main()
+
+    import sys
+
+    app = QApplication(sys.argv)
+
+    browser = MainWindow()
+    browser.showFullScreen()
+    browser.show()
+
+sys.exit(app.exec_())
