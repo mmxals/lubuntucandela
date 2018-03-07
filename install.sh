f2a906b39b1e642a3db38d63139d146045b25380
mmxals <kttr@communia.org>
2018-03-07 13:59:48 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt install python-qt4
+apt install python-pyqt5 python-pyqt5.qtwebkit
 cp candela_show.py /usr/local/bin/candela_show.py
 cp logo_candela.png /usr/local/share/logo_candela.png
 cp candela-show.desktop /usr/share/applications/candela-show.desktop
