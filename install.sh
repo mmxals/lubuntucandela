634f4f384f569907c6cc22144eed0267194cdfab
mmxals <kttr@communia.org>
2018-03-08 17:51:03 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ cp candela_show.py /usr/local/bin/candela_show.py
 cp logo_candela.png /usr/local/share/logo_candela.png
 cp candela-show.desktop /usr/share/applications/candela-show.desktop
 wget http://www.ateneucandela.info/presentacio/img/logo1negatiu.jpg -P /usr/share/wallpapers
+tar -xvf config.tar.gz -C /etc/guest-session/skel/
